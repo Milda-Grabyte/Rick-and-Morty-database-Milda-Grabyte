43b886c412d3ab9afc5875626d211b63c48f5474
Milda-Grabyte <milda.grabyte@gmail.com>
2020-11-24 03:36:42 +0100

    begin routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
 import '../styles/App.scss';
 import api from '../services/api';
 import CharacterList from './CharacterList';
 import Filters from './Filters';
+// import Detail from './CharacterDetail'
 
 function App() {
   const [characters, setCharacters] = useState([]);
@@ -22,18 +23,25 @@ function App() {
   const searchedCharacters = characters.filter(character => {
     return character.name.toLowerCase().includes(searchText.toLowerCase());
   });
-
+  console.log(characters);
    return (
      <>
-       <div className='main-container'>
-         <header>
-           <img src='https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png' alt='Rick and Morty logo'></img>
-         </header>
-         <main>
-           <Filters handleSearchText={handleSearchText} />
-           <CharacterList characters={searchText === '' ? characters : searchedCharacters} />
-         </main>
-       </div>
+       {/* <Switch> */}
+         <div className='main-container'>
+           <header>
+             <img
+               src='https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png'
+               alt='Rick and Morty logo'
+             ></img>
+           </header>
+           <main>
+             {/* <Route exact path="/" component={App} />
+             <Route path='/Detail' component={Detail} /> */}
+             <Filters handleSearchText={handleSearchText} />
+             <CharacterList characters={searchText === '' ? characters : searchedCharacters} />
+           </main>
+         </div>
+       {/* </Switch> */}
      </>
    );
 }
---
+++ src/components/CharacterDetail.js
@@ -0,0 +1,8 @@
+const Detail = () => {
+
+  return (
+   <h1>Detail page</h1>
+  );
+};
+
+export default Detail;
