e23a316cd23bfdceb4c8b1b9bc0b9da3047ade36
Milda-Grabyte <milda.grabyte@gmail.com>
2020-11-24 09:34:33 +0100

    merge from dev

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import '../styles/App.scss';
 import api from '../services/api';
 import CharacterList from './CharacterList';
