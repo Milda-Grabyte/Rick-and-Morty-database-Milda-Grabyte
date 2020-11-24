b95b826faa77b570a7f52406f39decfa5a6fb0b1
Milda-Grabyte <milda.grabyte@gmail.com>
2020-11-24 22:08:59 +0100

    update for docs

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -12,7 +12,12 @@ function CharacterList(props) {
   ) : (
     props.characters
       .sort((rick, morty) => (rick.name > morty.name ? 1 : rick.name === morty.name ? (rick.id > morty.id ? 1 : -1) : -1))
-      .map((character) => {
+        .map((character) => {
+          // let species;
+          // let status;
+          // if (character.species === "Human" && character.gender === "Male") {
+          //   species = ;
+          // }
         return (
           <li className='character__item' key={`character${character.id}`}>
             <CharacterCard
--- src/images/ImageList.js
+++ src/images/ImageList.js
@@ -3,3 +3,7 @@ export const logo = 'https://help.redbubble.com/hc/article_attachments/360002309
 export const facepalm = 'https://i.pinimg.com/originals/e4/48/63/e44863ebec8dd76213734a95fe6094f1.gif';
 
 export const notFound = 'https://cdn.dribbble.com/users/2795539/screenshots/5588688/im_sorry_but_your_opinion.png';
+
+export const dead = 'https://icon-library.com/images/death-icon-png/death-icon-png-21.jpg'
+
+export const alive = 'https://cdn1.iconfinder.com/data/icons/gamezzz/32/heart-512.png'
