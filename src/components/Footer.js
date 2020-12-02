4bc9abefd7061dbaf269a555ecefabe0c8f590ab
Milda-Grabyte <milda.grabyte@gmail.com>
2020-12-02 18:37:25 +0100

    improve layout and add sort by gender styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,11 @@ import '../styles/Footer.scss';
 function Footer() {
   return (
     <nav className='footer__nav'>
-      <p className='footer__text'>Made by</p>
+      <p className='footer__text-1'>Made by</p>
       <a className='footer__link footer__link--milda' href='https://github.com/Milda-Grabyte' target='_blank' rel='noreferrer'>
         Milda Grabyte
       </a>
-      <p className='footer__text'>2020</p>
+      <p className='footer__text-2'>2020</p>
       <a className='footer__link footer__link--adalab' href='https://adalab.es/' target='_blank' rel='noreferrer'>
         Adalab
       </a>
